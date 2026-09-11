4cf1e8b53f148fe9756a62854cae29be09d5e8d4
Shubhang Upadhyay <shubhupadhyay213@gmail.com>
2026-09-11 11:58:14 +0530

    exp1

--- exp1/exp1-2.js
+++ exp1/exp1-2.js
@@ -1,10 +1,10 @@
 const EventEmitter=require('events');
 const myEmitter= new EventEmitter();
 myEmitter.on('login', (name)=>{
-    console.log('$(name) Logged in successfully');
+    console.log('${name} Logged in successfully');
 });
 myEmitter.on('message', (msg)=>{
-    console.log('Message event received: $(msg)');
+    console.log('Message event received: ${msg}');
 });
 myEmitter.emit('login','Shubh');
 myEmitter.emit('message','Welcome to node.js');
